0d319ae4498c068156cf032deee8e9f68cbc59d8
sunstrix <apnogueira93@gmail.com>
2026-05-28 23:26:31 -0300

    Update UsuarioRepository.java

--- src/main/java/br/com/projetoA3/repository/UsuarioRepository.java
+++ src/main/java/br/com/projetoA3/repository/UsuarioRepository.java
@@ -1,1 +1,11 @@
-// TODO: Implementar repositório de Usuario 
+package br.com.projetoA3.repository;
+
+import br.com.projetoA3.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    
+    // ✅ Busca usuário pelo campo "login" (usado na autenticação)
+    Optional<Usuario> findByLogin(String login);
+}
