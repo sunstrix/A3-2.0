4000a7563f415fde721f2fe55ea1516b82dcb200
sunstrix <apnogueira93@gmail.com>
2026-05-28 23:25:41 -0300

    Correct package and formatting in A3Application
    Fix package declaration from 'br/com/projetoA3' to 'br.com.projetoA3' to use valid Java package syntax. Also tidy imports, spacing and indentation in src/main/java/br/com/projetoA3/A3Application.java; no functional changes.

--- src/main/java/br/com/projetoA3/A3Application.java
+++ src/main/java/br/com/projetoA3/A3Application.java
@@ -1,11 +1,12 @@
-package br/com/projetoA3; 
- 
-import org.springframework.boot.SpringApplication; 
-import org.springframework.boot.autoconfigure.SpringBootApplication; 
- 
-@SpringBootApplication 
-public class A3Application { 
-    public static void main(String[] args) { 
-        SpringApplication.run(A3Application.class, args); 
-    } 
-} 
+package br.com.projetoA3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A3Application.class, args);
+    }
+}
