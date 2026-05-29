f1278e70718094a6d1c47dc6b10e9ef59d6a050c
sunstrix <apnogueira93@gmail.com>
2026-05-29 09:36:24 -0300

    Update EquipeRepository.java
    feat(repository): adicionar repositório de Equipe com consulta por líder
    
    Implementa a interface JpaRepository para a entidade Equipe. Inclui o
    método derivado findByLiderId para facilitar consultas de equipes
    associadas a um usuário líder específico.

--- src/main/java/br/com/projetoA3/repository/EquipeRepository.java
+++ src/main/java/br/com/projetoA3/repository/EquipeRepository.java
@@ -1,1 +1,11 @@
-// TODO: Implementar repositório de Equipe 
+package br.com.projetoA3.repository;
+
+import br.com.projetoA3.model.Equipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface EquipeRepository extends JpaRepository<Equipe, Long> {
+    
+    // ✅ Buscar equipes filtradas pelo líder responsável
+    List<Equipe> findByLiderId(Long liderId);
+}
